617989d2cd678329be90058bca8c0eb62d7ea1f9
Ray Olivier <ray.olivier@projekt202.com>
2019-02-20 13:16:40 -0600

    setting up the fake api thingy

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,7 @@ import { HEROES } from "./mock-heroes";
 import { Hero } from "./hero";
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 // The @Injectable() decorator accepts a metadata object for the service, the same way the @Component() decorator did for your component classes.
 
@@ -11,17 +12,27 @@ import { Observable, of } from "rxjs";
   // When you provide the service at the root level, Angular creates a single, shared instance of HeroService and injects into any class that asks for it. Registering the provider in the @Injectable metadata also allows Angular to optimize an app by removing the service if it turns out not to be used after all.
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
+
+  private heroesUrl = "api/heroes"; // URL to web api
+
   // This is a typical "service-in-service" scenario: you inject the MessageService into the HeroService which is injected into the HeroesComponent.
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.messageService.add("HeroService: fetched heroes");
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find((hero) => hero.id === id));
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
